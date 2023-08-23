6517d04623bd0611e8e01c160b9247756578412d
Hugo Marques <hellohugomarques@gmail.com>
2023-08-22 21:16:12 -0500

    Migrate to WebFlux

--- src/main/java/com/hugomarques/rinhabackend2023/RinhaBackend2023Application.java
+++ src/main/java/com/hugomarques/rinhabackend2023/RinhaBackend2023Application.java
@@ -3,11 +3,13 @@ package com.hugomarques.rinhabackend2023;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableCaching
-@EnableAsync
+@EnableR2dbcRepositories
 public class RinhaBackend2023Application {
 
 	public static void main(String[] args) {
